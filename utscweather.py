81744a8418256cb5d92a40c0af02fd00b68ed51d
Hanno Rein <hanno@hanno-rein.de>
2015-03-07 23:33:24 -0500

    pull and display

--- utscweather.py
+++ utscweather.py
@@ -1,12 +1,16 @@
 from functools import wraps
 from flask import Flask, abort, render_template, send_from_directory, request, redirect, Response
+import urllib2
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index.html')
 def page_main():
+    current_conditions = urllib2.urlopen("http://weather.utsc.utoronto.ca/current_conditions.txt").read().split("\n")[6:17]
+
     return render_template("index.html",
+        current_conditions = current_conditions,
         )
 
 if __name__ == '__main__':
