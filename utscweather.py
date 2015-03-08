da56ebda223fc0c4c5d1399a70e96d4ecf98584e
Hanno Rein <hanno@hanno-rein.de>
2015-03-08 01:00:48 -0500

    temperature plot working

--- utscweather.py
+++ utscweather.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import Flask, abort, render_template, send_from_directory, request, redirect, Response
 import urllib2
+import json
 
 app = Flask(__name__)
 
@@ -11,12 +12,19 @@ def page_main():
     rawdata = rawdata.replace("deg C","&#176;C")
     rawdata = rawdata.replace("deg","&#176;")
     rawdata = rawdata.replace("^2","&#178;")
-    
     rawdata = rawdata.split("\n")[6:17]
+
+    hourly = urllib2.urlopen("http://weather.utsc.utoronto.ca/Hourly_current_month.dat").read().strip().split("\n")[4:]
     
+    temp_array = []
+    for h in hourly[-48:]:
+        fields = h.split(",")
+        temp_array.append(float(fields[2]))
+     
 
     return render_template("index.html",
         current_conditions = rawdata,
+        temp_array = json.dumps(temp_array),
         )
 
 if __name__ == '__main__':
