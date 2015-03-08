4a22cdc6a5ab44fadcaf35a6d391f99fbbc39848
Hanno Rein <hanno@hanno-rein.de>
2015-03-07 23:42:26 -0500

    units

--- utscweather.py
+++ utscweather.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def page_main():
-    current_conditions = urllib2.urlopen("http://weather.utsc.utoronto.ca/current_conditions.txt").read().split("\n")[6:17]
+    rawdata = urllib2.urlopen("http://weather.utsc.utoronto.ca/current_conditions.txt").read()
+    rawdata = rawdata.replace("deg C","&#176;C")
+    rawdata = rawdata.replace("deg","&#176;")
+    rawdata = rawdata.replace("^2","&#178;")
+    
+    rawdata = rawdata.split("\n")[6:17]
+    
 
     return render_template("index.html",
-        current_conditions = current_conditions,
+        current_conditions = rawdata,
         )
 
 if __name__ == '__main__':
