9afc8988d7c5aeb9f022c921d3eed0faf2aa8265
Hanno Rein <hanno@hanno-rein.de>
2021-03-22 12:22:55 -0400

    updated url and mapping

--- utscweather.py
+++ utscweather.py
@@ -8,33 +8,33 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def page_main():
-    rawdata = urllib2.urlopen("http://weather.utsc.utoronto.ca/current_conditions.txt").read()
+    rawdata = urllib2.urlopen("https://weather.utsc.utoronto.ca/data/current_conditions.txt").read()
     rawdata = rawdata.replace("deg C","&#176;C")
     rawdata = rawdata.replace("deg","&#176;")
     rawdata = rawdata.replace("^2","&#178;")
     rawdata = rawdata.split("\n")[6:17]
 
-    hourly = urllib2.urlopen("http://weather.utsc.utoronto.ca/Hourly_current_month.dat").read().strip().split("\n")[4:]
+    hourly = urllib2.urlopen("https://weather.utsc.utoronto.ca/data/Hourly_current_month.dat").read().strip().split("\n")[4:]
     
     data = []
 
     mapper = {
         0:2,   #temp
-        1:3,   #rel
-        2:5,   #baro
-        3:7,   #wind speed
-        4:8,   #wind direction
-        5:6,   #rain
-        6:9,   #solar
-        7:10,   #sky
-        8:11,   #hum ind
-        9:4,   #dew
-        10:12,   #wind chill
+        1:8,   #rel
+        2:9,   #baro
+        3:10,   #wind speed
+        4:11,   #wind direction
+        5:12,   #rain
+        6:13,   #solar
+        7:14,   #sky
+        8:15,   #hum ind
+        9:7,   #dew
+        10:16,   #wind chill
     }
     for i,d in enumerate(rawdata):
         temp_array = []
         for h in hourly[-24*7:]:
-            fields = h.split(",")
+            fields = h.strip().split(",")
             temp_array.append({"date":fields[0].strip("\""), "value":float(fields[mapper[i]])})
         data.append([d,json.dumps(temp_array)])
 
